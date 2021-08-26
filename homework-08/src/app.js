ea105f453fa67ce48ea37b33afa2a83959aa2ec5
bod111 <86015208+bod111@users.noreply.github.com>
2021-08-26 17:11:47 +0300

    Update app.js

--- homework-08/src/app.js
+++ homework-08/src/app.js
@@ -63,3 +63,68 @@ const galleryItems = [
     description: 'Lighthouse Coast Sea',
   },
 ];
+  
+// const allImg = document.querySelectorAll('img');
+// const sec = document.querySelectorAll('.js_container_img');
+
+// const fnEntry = (entries, observer) => {
+//   // console.log('observer', observer);
+
+//   if (entries[0].isIntersecting) {
+//     const img = entries[0].target;
+//     const needSrc = img.dataset.needsrc;
+//     img.src = needSrc;
+//     observer.disconnect();
+//   }
+// };
+
+// const addIO = img => {
+//   const intOb = new IntersectionObserver(fnEntry);
+//   intOb.observe(img);
+// };
+
+// allImg.forEach(img => addIO(img));
+const refs = {
+  jsLightBox: document.querySelector('.js-lightbox'),
+  lightBoxOverlay: document.querySelector('.lightbox__overlay'),
+  lightBoxContent: document.querySelector('.lightbox__content'),
+  lightBoxImage: document.querySelector('.lightbox__image'),
+  lightBoxButton: document.querySelector('.lightbox__button'),
+  gallery: document.querySelector('.js-gallery')
+}
+
+const createGallaryMarkup = galleryItems.map(({ preview, original, description }) => {
+    return`<li class="gallery__item">
+      <a class ="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/>
+      </a>
+     </li>`;
+  }).join('');
+
+refs.gallery.insertAdjacentHTML('beforeend', createGallaryMarkup);
+// ------ modal----------------
+refs.gallery.addEventListener('click', onPictureClick)
+
+function onPictureClick(e) {
+e.preventDefault();
+  if (!e.target.classList.contains('gallery__image')) {
+  return
+}
+  refs.jsLightBox.classList.add('is-open');
+  refs.lightBoxImage.src = e.target.dataset.source;
+}
+
+refs.jsLightBox.addEventListener('click', onModalCloseBtnClick)
+function onModalCloseBtnClick(e) {
+  if (e.target.classList.contains('lightbox__button')) {
+    closeModal();
+  }
+}
+
+function closeModal(e) {
+  refs.lightBoxImage.src = '';
+  refs.jsLightBox.classList.remove('is-open');
+}
+
+
+
